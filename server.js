cee1fab092c880641b44e20634b46b1a8be50835
Wonsik Jung <ws@WS-MacBook.local>
2020-12-08 10:13:29 +0900

    first upload

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
-// test
-console.log("test sys");
-// ho ho ho from santa
+var path = require('path');
+var express = require('express');
+var exphbs=require('express-handlebars');
+
+console.log('Data:',Database);
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars',exphbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('/',function(req,res,next){
+    res.status(200).render('home_page');
+});
