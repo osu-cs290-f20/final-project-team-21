3fdd4527b70016556646476944efef26a124be41
chaisupt <72107485+chaisupt@users.noreply.github.com>
2020-12-09 02:53:43 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
 
-console.log('Data:',Database);
+
 var app = express();
 var port = process.env.PORT || 3000;
 
@@ -11,6 +11,33 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
+app.listen(port, function () {
+    console.log("\n== Server is listening on port", port);
+    console.log("\nworking ")
+  });
+
 app.get('/',function(req,res,next){
     res.status(200).render('home_page');
 });
+
+app.get('/create',function(req,res,next){
+    res.status(200).render('create_page');
+});
+
+app.get('/share',function(req,res,next){
+  res.status(200).render('share_creator');
+});
+
+app.get('/play',function(req,res,next){
+  res.status(200).render('play_main');
+});
+
+app.get('/done',function(req,res,next){
+  res.status(200).render('play_done');
+});
+
+app.get('*', function (req, res) {
+    // res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+    res.status(404).render('404');
+  });
+
