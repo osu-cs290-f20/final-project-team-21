1163ab609b103c02f18b650fae87b3317c94176e
Youjin814 <72735181+Youjin814@users.noreply.github.com>
2020-12-10 16:45:09 -0800

    Update server.js

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ app.get('/create',function(req,res,next){
     res.status(200).render('create_page');
 });
 
-app.get('/share',function(req,res,next){
-  res.status(200).render('share_creator');
-});
+//app.get('/share',function(req,res,next){
+//  res.status(200).render('share_creator');
+//});
 
 app.get('/:n',function(req,res,next){
   var n_array=req.params.n;
@@ -38,17 +38,17 @@ app.get('/:n',function(req,res,next){
   next();
 });
 
-app.get('/play',function(req,res,next){
-  res.status(200).render('play_main');
-});
+//app.get('/play',function(req,res,next){
+//  res.status(200).render('play_main');
+//});
 
 app.get('/enter',function(req,res,next){
   res.status(200).render('enter_code');
 });
 
-app.get('/done',function(req,res,next){
-  res.status(200).render('play_done');
-});
+//app.get('/done',function(req,res,next){
+//  res.status(200).render('play_done');
+//});
 
 app.get('*', function (req, res) {
     // res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
